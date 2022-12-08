d76882e7656a06a87b0423ed2b63f49d5035d908
João Victor <joaochacon1234@gmail.com>
2022-12-08 01:28:09 -0300

    update

--- src/Componentes/QuemSouEu/index.jsx
+++ src/Componentes/QuemSouEu/index.jsx
@@ -10,7 +10,7 @@ export default function SobreInicial() {
         </div>
         <div className='SobreInicial__conteudo'>
             <h2>Olá, meu nome é<br/> João Victor Chacon</h2>
-            <p>Estou cursando atualmente o 4° semestre de Análise e Desenvolvimento de Sistemas. Tive um amor a primeira vista pela área de front-end assim que comecei o curso, e tenho como meta trabalhar nessa área. FALTA ADICIONAR MAIS COISAS</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod enim libero, at mattis odio tincidunt at. Proin ex metus, laoreet quis risus et, scelerisque mattis est. Aliquam erat volutpat. Morbi consectetur porta tellus ut malesuada. Vestibulum pretium arcu nisl. Phasellus malesuada posuere ultricies. Vivamus feugiat euismod velit non auctor. Nulla a semper tortor.</p>
         </div>
     </div>
   )
