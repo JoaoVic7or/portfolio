4cfe4464b4d2c07cd846fa3d3a790e6fc6c0182e
João Victor <52728391+JoaoVic7or@users.noreply.github.com>
2023-01-04 17:42:45 -0300

    update

---
+++ src/Paginas/PaginaError/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PaginaError() {
+  return (
+    <div>
+      <img src='https://www.github.com/JoaoVic7or.png'/>
+      <span>Oops, essa página não existe...</span>
+    </div>
+  )
+}
